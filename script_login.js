790287a4dc05910397fdca1d5f4773d5bef2c2df
RogelioIRamirez <rogelio.ramirez2567@alumnos.udg.mx>
2023-12-03 15:17:07 -0600

    update

--- script_login.js
+++ script_login.js
@@ -7,5 +7,22 @@ function submitForm() {
     } else {
         // Aquí puedes redirigir al usuario a la página de la encuesta o realizar otras acciones.
         alert("Inicio de sesión exitoso. Redirigiendo a la encuesta...");
+        window.location.href = "main_page.html";
+    }
+}
+
+function registerUser() {
+    var newUsername = document.getElementById("newUsername").value;
+    var newPassword = document.getElementById("newPassword").value;
+
+    if (newUsername === "" || newPassword === "") {
+        alert("Por favor, completa todos los campos.");
+    } else {
+        // Aquí puedes agregar la lógica para almacenar el nuevo usuario
+        // por ejemplo, podrías usar un array en memoria para este ejemplo
+        var newUser = { username: newUsername, password: newPassword };
+        users.push(newUser);
+
+        alert("Registro exitoso. Ahora puedes iniciar sesión.");
     }
 }
